dc921e0482585ea7a4cdf1d83fe64586d5777f5e
DZonLive <david.zakiyanc@gmail.com>
2022-10-20 18:51:42 +0300

    1

--- py/random_scripts.py
+++
@@ -1,9 +0,0 @@
-try:
-	with open('text.txt', 'wt', encoding='utf-8') as inFile:
-		num = int(input())
-		result = str('1 / ' + str(num) + ' = ' + str(1 / num))
-		print(result)
-		inFile.write(result)
-except ValueError:
-    num = 0
-    print('Вы ввели текст.')
