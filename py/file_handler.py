2b06b7a4d0ba9d893d95f4324918c10a6ff7b845
DZonLive <david.zakiyanc@gmail.com>
2022-11-14 15:57:35 +0300

    2

--- py/file_handler.py
+++ py/file_handler.py
@@ -7,7 +7,7 @@ class Handler(FileSystemEventHandler):
     def on_modified(self, event):
         for filename in os.listdir(folder_track):
             extension = filename.split(".")
-            if len(extension) > 1 and (extension[1].lower() == "jpg" or extension[1].lower() == "png" or extension[1].lower() == "svg") 
+            if len(extension) > 1 and (extension[1].lower() == "jpg" or extension[1].lower() == "png" or extension[1].lower() == "svg"):
                 file = folder_track + "/" + filename
                 new_path = folder_dest + "/" + filename
                 os.rename(file, new_path)s
